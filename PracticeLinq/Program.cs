55528b0c4c329d374f586cf6b50236a1c08897a5
JulianRenaud <midway6220@gmail.com>
2022-09-30 10:14:21 -0500

    First Commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+List<string> games = new List<String>() 
+{
+    "Minecraft",
+    "Roblox",
+    "Rocket League",
+    "Call Of Duty",
+    "Starcraft II",
+    "Fall Guys",
+    "Destiny 2",
+    "Fortnite"
+};
+
+games
+    .OrderBy(x => x.Length)
+    .ThenBy(x => x)
+    .ToList()
+    .ForEach(x => Console.WriteLine(x));
+
+
+//Create a new console app and name it PracticeLinq.
+//Create a list of video game names...
+//Order the list of games by length of the game name.
+//Use the lambda expression in this exercise as well.
+//use Method Syntax for this exercise
